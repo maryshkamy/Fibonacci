b2d2a033d3d47f45f916bf9a8b92caae3af7a6a5
Mariana Rios Silveira Carvalho <marianariossc@gmail.com>
2017-09-19 21:32:03 -0300

    Update
    Fixed some problems.

--- Fibonacci.playground/Contents.swift
+++ Fibonacci.playground/Contents.swift
@@ -4,18 +4,18 @@ import UIKit
 
 print("Fibonacci\n")
 
-func Fibonacci(N: Int) -> Int{
-    if N <= 1{
+func Fibonacci(N: Int) -> Int {
+    if N <= 1 {
         return N
     }
-    else{
-        return Fibonacci(N - 1) + Fibonacci(N - 2)
+    else {
+        return Fibonacci(N: N - 1) + Fibonacci(N: N - 2)
     }
 }
 
 var Number = 14
 
-for i in 1...Number{
-    let fibonacci = Fibonacci(i)
+for i in 1...Number {
+    let fibonacci = Fibonacci(N: i)
     print("\(i) = \(fibonacci)")
 }
